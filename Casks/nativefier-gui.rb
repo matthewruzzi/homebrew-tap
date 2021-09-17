626b04dc2975c652abb7718b191369dd169f277c
Matthew Ruzzi <68619790+mattruzzi@users.noreply.github.com>
2021-09-16 18:12:18 -0700

    nativefier-gui v0.2.2

--- Casks/nativefier-gui.rb
+++ Casks/nativefier-gui.rb
@@ -1,11 +1,11 @@
 cask "nativefier-gui" do
-  version "0.2.1"
+  version "0.2.2"
 
   if Hardware::CPU.intel?
-    sha256 "2d370e4f9fa003de643bea0863266d0b9b36554432949f0acdc0c3bd0960a913"
+    sha256 "60a5e2d9e1dedff5f0f49fdace83b8a167d8de8bc1b5ab6cdaa4b1777566758f"
     url "https://github.com/mattruzzi/nativefier-gui/releases/download/#{version}/nativefier-gui-darwin-x64.zip"
   else
-    sha256 "241673291c9a139ea589299018e01413defc4549db23e48079e9ebe653cf268e"
+    sha256 "741a688086b9305896b3120f7e85362bd8188a4812486dd177d58ce6ceb3b8a3"
     url "https://github.com/mattruzzi/nativefier-gui/releases/download/#{version}/nativefier-gui-darwin-arm64.zip"
   end
 
