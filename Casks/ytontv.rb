fbe59dfc3547fac3e9157ccc2ac4dc5a7edb2639
Matthew Ruzzi <68619790+mattruzzi@users.noreply.github.com>
2022-09-21 19:00:04 -0700

    ytontv v50.0.0,20.2.0

--- Casks/ytontv.rb
+++ Casks/ytontv.rb
@@ -1,12 +1,12 @@
 cask "ytontv" do
-  version "49.0.1,20.1.1"
+  version "50.0.0,20.2.0"
 
   if Hardware::CPU.intel?
-    sha256 "aa3267d894bdad7b78fe05e5b2cddf0935227af9d44a792607ed0de1c7442571"
+    sha256 "8b1d171165a56a7221d6253011842746e871a2e47ee18491612bc211d2b69ecb"
     url "https://github.com/mattruzzi/Nativefier-YouTube-on-TV-for-Desktop/releases/download/#{version}/YouTubeonTV-darwin-x64.zip"
     app "YouTubeonTV-darwin-x64/YouTube\ on\ TV.app"
   else
-    sha256 "4df9b66a064ca1d19a585f1514050a067ce906af84937130e71acc79f68be77c"
+    sha256 "6d4af449dadd365b5a5fb29bb8fbecf1d659d1b3eb2310fd25e7767e9abe7be9"
     url "https://github.com/mattruzzi/Nativefier-YouTube-on-TV-for-Desktop/releases/download/#{version}/YouTubeonTV-darwin-arm64.zip"
     app "YouTubeonTV-darwin-arm64/YouTube\ on\ TV.app"
   end
