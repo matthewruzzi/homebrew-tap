c2ed544418a7e395a291f4519aca32dd458030b6
Matthew Ruzzi <68619790+mattruzzi@users.noreply.github.com>
2023-09-01 15:23:41 -0700

    Change from Hardware::CPU.intel? to on_intel do

--- Casks/nativefier-gui.rb
+++ Casks/nativefier-gui.rb
@@ -1,10 +1,12 @@
 cask "nativefier-gui" do
   version "0.8.0"
 
-  if Hardware::CPU.intel?
+  on_intel do
     sha256 "a8aed11e41ebaf2dfdd5f8fc1a21c8ea53b78d650029837fbb7687c27e7fa3c1"
     url "https://github.com/mattruzzi/nativefier-gui/releases/download/#{version}/nativefier-gui-darwin-x64.zip"
-  else
+  end
+  
+  on_arm do
     sha256 "d9fdc69cf43d3b810ccf4bbab0584f92eeaed5fd5f8b3a0455174e199ff54576"
     url "https://github.com/mattruzzi/nativefier-gui/releases/download/#{version}/nativefier-gui-darwin-arm64.zip"
   end
--- Casks/ytontv.rb
+++ Casks/ytontv.rb
@@ -1,11 +1,13 @@
 cask "ytontv" do
   version "50.1.1,22.3.15"
 
-  if Hardware::CPU.intel?
+  on_intel do
     sha256 "48f4279f8559f56488abda22eb3a8f9ea3e133706d0fdd1096afcace19b3ee55"
     url "https://github.com/mattruzzi/Nativefier-YouTube-on-TV-for-Desktop/releases/download/#{version}/YouTubeonTV-darwin-x64.zip"
     app "YouTubeonTV-darwin-x64/YouTube\ on\ TV.app"
-  else
+  end
+
+  on_arm do
     sha256 "965d4750b924ba3bb8a24dfd26cb89876b633c311c7cd189ae447b24420bc987"
     url "https://github.com/mattruzzi/Nativefier-YouTube-on-TV-for-Desktop/releases/download/#{version}/YouTubeonTV-darwin-arm64.zip"
     app "YouTubeonTV-darwin-arm64/YouTube\ on\ TV.app"
