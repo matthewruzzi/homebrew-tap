f6a0b9a701857cc31d83480830ed1f90eae588da
Matthew Ruzzi <68619790+mattruzzi@users.noreply.github.com>
2023-09-01 15:30:31 -0700

    Fix style issues

--- Casks/nativefier-gui.rb
+++ Casks/nativefier-gui.rb
@@ -1,26 +1,28 @@
 cask "nativefier-gui" do
   version "0.8.0"
 
-  on_intel do
-    sha256 "a8aed11e41ebaf2dfdd5f8fc1a21c8ea53b78d650029837fbb7687c27e7fa3c1"
-    url "https://github.com/mattruzzi/nativefier-gui/releases/download/#{version}/nativefier-gui-darwin-x64.zip"
-  end
-  
   on_arm do
     sha256 "d9fdc69cf43d3b810ccf4bbab0584f92eeaed5fd5f8b3a0455174e199ff54576"
+
     url "https://github.com/mattruzzi/nativefier-gui/releases/download/#{version}/nativefier-gui-darwin-arm64.zip"
+  end
+  on_intel do
+    sha256 "a8aed11e41ebaf2dfdd5f8fc1a21c8ea53b78d650029837fbb7687c27e7fa3c1"
+
+    url "https://github.com/mattruzzi/nativefier-gui/releases/download/#{version}/nativefier-gui-darwin-x64.zip"
   end
 
   name "Nativefier GUI"
   desc "Make any web page a desktop application"
   homepage "https://github.com/mattruzzi/nativefier-gui"
-  app "nativefier-gui.app"
 
   livecheck do
     url :stable
     strategy :github_latest
   end
-  
+
+  app "nativefier-gui.app"
+
   postflight do
     system "xattr", "-r", "-d", "com.apple.quarantine", "#{appdir}/nativefier-gui.app"
   end
--- Casks/ytontv.rb
+++ Casks/ytontv.rb
@@ -1,16 +1,19 @@
 cask "ytontv" do
   version "50.1.1,22.3.15"
 
-  on_intel do
-    sha256 "48f4279f8559f56488abda22eb3a8f9ea3e133706d0fdd1096afcace19b3ee55"
-    url "https://github.com/mattruzzi/Nativefier-YouTube-on-TV-for-Desktop/releases/download/#{version}/YouTubeonTV-darwin-x64.zip"
-    app "YouTubeonTV-darwin-x64/YouTube\ on\ TV.app"
-  end
-
   on_arm do
     sha256 "965d4750b924ba3bb8a24dfd26cb89876b633c311c7cd189ae447b24420bc987"
+
     url "https://github.com/mattruzzi/Nativefier-YouTube-on-TV-for-Desktop/releases/download/#{version}/YouTubeonTV-darwin-arm64.zip"
-    app "YouTubeonTV-darwin-arm64/YouTube\ on\ TV.app"
+
+    app "YouTubeonTV-darwin-arm64/YouTube on TV.app"
+  end
+  on_intel do
+    sha256 "48f4279f8559f56488abda22eb3a8f9ea3e133706d0fdd1096afcace19b3ee55"
+
+    url "https://github.com/mattruzzi/Nativefier-YouTube-on-TV-for-Desktop/releases/download/#{version}/YouTubeonTV-darwin-x64.zip"
+
+    app "YouTubeonTV-darwin-x64/YouTube on TV.app"
   end
 
   name "YouTube on TV.app"
@@ -21,7 +24,7 @@ cask "ytontv" do
     url :stable
     strategy :github_latest
   end
-  
+
   postflight do
     system "xattr", "-r", "-d", "com.apple.quarantine", "#{appdir}/YouTube on TV.app"
   end
