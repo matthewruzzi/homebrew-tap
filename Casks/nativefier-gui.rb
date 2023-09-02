f27f4b4ad8d2ca8c0058880f8643178039f74a6c
Matthew Ruzzi <68619790+mattruzzi@users.noreply.github.com>
2023-09-01 17:10:15 -0700

    Fix app stanza in nativefier-gui.rb

--- Casks/nativefier-gui.rb
+++ Casks/nativefier-gui.rb
@@ -5,11 +5,15 @@ cask "nativefier-gui" do
     sha256 "4fefb462d78edf50a66d1085a21303cd70f2c6d9a46185c3bad6db3bea4963b9"
 
     url "https://github.com/mattruzzi/nativefier-gui/releases/download/#{version}/nativefier-gui-darwin-arm64.zip"
+
+    app "nativefier-gui-darwin-arm64/nativefier-gui.app"
   end
   on_intel do
     sha256 "5273835199059e43aa863375e798fa28625b3fb6a5e62a131dcd43a6457d2837"
 
     url "https://github.com/mattruzzi/nativefier-gui/releases/download/#{version}/nativefier-gui-darwin-x64.zip"
+
+    app "nativefier-gui-darwin-x64/nativefier-gui.app"
   end
 
   name "Nativefier GUI"
@@ -20,8 +24,6 @@ cask "nativefier-gui" do
     url :stable
     strategy :github_latest
   end
-
-  app "nativefier-gui.app"
 
   postflight do
     system "xattr", "-r", "-d", "com.apple.quarantine", "#{appdir}/nativefier-gui.app"
